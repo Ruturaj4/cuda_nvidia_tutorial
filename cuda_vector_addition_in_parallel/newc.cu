25d99e4ff517156edb00c88aa034e802e21bd4bc
Ruturaj4 <ruturajkvaidya@gmail.com>
2018-03-23 00:27:08 -0500

    Add files via upload

---
+++ cuda_vector_addition_in_parallel/newc.cu
@@ -0,0 +1,58 @@
+#include "stdio.h"
+
+#define N 512
+
+__global__ void add(int *a, int *b, int *c)
+{
+
+c[blockIdx.x] = a[blockIdx.x] + b[blockIdx.x];
+
+}
+
+void random_ints(int* a, int n)
+{
+
+int i;
+for(i =0; i<n; ++i){
+a[i]=rand()/2000;
+}
+
+}
+
+int main(void)
+{
+
+int *a,*b,*c;
+int *d_a, *d_b, *d_c;
+int size = N * sizeof(int);
+
+cudaMalloc((void**)&d_a, size);
+cudaMalloc((void**)&d_b, size);
+cudaMalloc((void**)&d_c, size);
+
+a = (int*)malloc(size);
+random_ints(a, N);
+b = (int*)malloc(size);
+random_ints(b, N);
+c = (int*)malloc(size);
+
+cudaMemcpy(d_a, a, size, cudaMemcpyHostToDevice);
+cudaMemcpy(d_b, b, size, cudaMemcpyHostToDevice);
+
+add<<<N,1>>>(d_a, d_b, d_c);
+
+cudaMemcpy(c, d_c, size, cudaMemcpyDeviceToHost);
+
+printf("%d + %d is %d\n", *a, *b, *c);
+
+free(a);
+free(b);
+free(c);
+
+cudaFree(d_a);
+cudaFree(d_b);
+cudaFree(d_c);
+
+return 0;
+
+}
